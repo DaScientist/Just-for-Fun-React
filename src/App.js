9b4b2c5e74200f903e5ac3879f70cd4f5a97837a
DaScientist <mustanseer.170410107097@gmail.com>
2022-06-11 22:18:53 +0530

    just trying a stunt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidUpdate() {
+    console.log("object");
+    this.setState({});
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img
+            onClick={() => {
+              console.log("Hello");
+              this.setState({});
+            }}
+            src={logo}
+            className="App-logo"
+            alt="logo"
+          />
+          <p>
+            Edit <code> src / App.js </code> and save to reload.{" "}
+          </p>{" "}
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React{" "}
+          </a>{" "}
+        </header>{" "}
+        <button type="button" onClick={() => {
+              console.log("Hello");
+              this.setState({});
+            }}>Hello world</button>
+      </div>
+    );
+  }
 }
 
 export default App;
